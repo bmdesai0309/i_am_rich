ed0e9cd22c3a6b1fc820c257d5d86b49e59fe9e2
bmdesai0309 <bmdesai1685@gmail.com>
2020-12-12 09:55:34 +0530

    add background and change color of appbar

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,13 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text('Hello World'),
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: Text('I Am Rich'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
       ),
-    ),
+      )
   );
 }
