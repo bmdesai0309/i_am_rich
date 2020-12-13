6265b204d72045e364e160a52ac240d71ef45a3d
bmdesai0309 <bmdesai1685@gmail.com>
2020-12-13 16:06:48 +0530

    use asset image instead of network image and add assets in pubspec.yaml file

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ void main() {
         ),
         body: Center(
           child: Image(
-            image: NetworkImage('https://cdn.arstechnica.net/wp-content/uploads/2016/02/5718897981_10faa45ac3_b-640x624.jpg'),
+            image: AssetImage('images/diamond.png'),
           ),
         ),
       ),
