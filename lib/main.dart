188fa9c9dfdd7bb1bfc7f2db677e7491f68ddff0
bmdesai0309 <bmdesai1685@gmail.com>
2020-12-13 15:56:23 +0530

    add network image from the internet in the image widget. Create a new directory called image and add dimond image in it.

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,11 @@ void main() {
           title: Text('I Am Rich'),
           backgroundColor: Colors.blueGrey[900],
         ),
+        body: Center(
+          child: Image(
+            image: NetworkImage('https://cdn.arstechnica.net/wp-content/uploads/2016/02/5718897981_10faa45ac3_b-640x624.jpg'),
+          ),
+        ),
       ),
       )
   );
